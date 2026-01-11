9a16c4885e239c621436d29354c56bd31a25af01
Subhamay-Dey <ncdey1966@gmail.com>
2026-01-11 23:59:56 +0530

    refactor: add conditional rendering for TabIcon component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,15 +9,19 @@ const TabIcon = ({focused, icon, tab}: {icon: ImageSourcePropType, tab: string, 
 
   const props = {icon, tab}
 
-  return (
-    <ImageBackground
-      source={images.highlight}
-      className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
-    >
-      <Image source={props.icon} tintColor="#151312" className='size-5'/>
-      <Text className='text-secondary text-base font-semibold ml-2'>{props.tab}</Text>
-    </ImageBackground>
-  )
+  if (!focused) {
+
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+      >
+        <Image source={props.icon} tintColor="#151312" className='size-5'/>
+        <Text className='text-secondary text-base font-semibold ml-2'>{props.tab}</Text>
+      </ImageBackground>
+    )
+  }
+
 }
 
 const _layout = () => {
