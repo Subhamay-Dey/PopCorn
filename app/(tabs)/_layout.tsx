5ce573ab620a9293cf0074c382430d1bd1a6bfb9
Subhamay-Dey <ncdey1966@gmail.com>
2026-01-12 01:47:54 +0530

    updated TabIcon styles and enhance tab bar options for better layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,7 +13,7 @@ const TabIcon = ({focused, icon, tab}: {icon: ImageSourcePropType, tab: string, 
     return (
       <ImageBackground
         source={images.highlight}
-        className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+        className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
       >
         <Image source={props.icon} tintColor="#151312" className='size-5'/>
         <Text className='text-secondary text-base font-semibold ml-2'>{props.tab}</Text>
@@ -31,7 +31,28 @@ const TabIcon = ({focused, icon, tab}: {icon: ImageSourcePropType, tab: string, 
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: '100%',
+          height: '100%',
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: 'absolute',
+          overflow: 'hidden',
+          borderWidth: 1,
+          borderColor: "0f0d23",
+        }
+      }}
+    >
         <Tabs.Screen
             name='index'
             options={{
